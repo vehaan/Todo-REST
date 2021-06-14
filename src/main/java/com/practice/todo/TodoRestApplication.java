8a2c10c3051efa75daf774205d036f16c225fd7b
Vehaan Singh Kundra <vehaanskundra@gmail.com>
2021-06-14 18:38:41 +0530

    added swagger

--- src/main/java/com/practice/todo/TodoRestApplication.java
+++ src/main/java/com/practice/todo/TodoRestApplication.java
@@ -1,13 +1,47 @@
 package com.practice.todo;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TodoRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoRestApplication.class, args);
+		
 	}
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.practice"))
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	      "Todo List", 
+	      "API for Todo List", 
+	      "1.0", 
+	      "https://www.google.com/", 
+	      new Contact("Vehaan Singh Kundra", "https://www.google.com/", "vehaanskundra@gmail.com"), 
+	      "License of API", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+	}
+	
 
 }
--- src/main/java/com/practice/todo/controller/TaskController.java
+++ src/main/java/com/practice/todo/controller/TaskController.java
@@ -8,18 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.practice.todo.entity.Task;
 import com.practice.todo.service.TaskServiceImpl;
 
 @RestController
-//@RequestMapping("/")
 public class TaskController {
 	
 	@Autowired
@@ -31,13 +29,13 @@ public class TaskController {
 		return new ResponseEntity<Task>(newTask,HttpStatus.OK);
 	}
 	
-	@RequestMapping("/tasks")
+	@GetMapping("/tasks")
 	public ResponseEntity<List<Task>> allTask() throws Exception{
 		List<Task> tasks = taskService.getAllTask();
 		return new ResponseEntity<List<Task>>(tasks,HttpStatus.OK);
 	}
 	
-	@RequestMapping("/tasks/{id}")
+	@GetMapping("/tasks/{id}")
 	public ResponseEntity<Task> getTask(@PathVariable int id){
 		Task task = taskService.getTask(id);
 		return new ResponseEntity<Task>(task,HttpStatus.OK);
